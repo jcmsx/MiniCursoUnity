10fb78c1267b69b3476b0b4e9b7773728f3508a5
jcmsx <jcmsx@yahoo.com.br>
2022-07-17 19:47:23 -0300

    config mecanica inicial player

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+
+	private Rigidbody2D playerRb;
+
+	public float speed;
+	public float jumpForce;
+
+	public bool isLookLeft;
+
+	// Use this for initialization
+	void Start () {
+	
+		playerRb = GetComponent<Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float h = Input.GetAxisRaw ("Horizontal");
+
+
+		if (h > 0 && isLookLeft == true) {
+			Flip ();
+
+		} else if (h < 0 && isLookLeft == false) {
+			Flip ();
+		}
+
+
+		float speedY = playerRb.velocity.y;
+
+		if (Input.GetButtonDown ("Jump")) {
+
+			playerRb.AddForce( new Vector2(0,jumpForce));
+
+		}
+	
+
+		playerRb.velocity = new Vector2 (h * speed, speedY);
+	}
+
+	void Flip()
+	{
+
+
+		isLookLeft = !isLookLeft;
+
+		float x = transform.localScale.x * -1; // inverte sinal do scale x 
+
+		transform.localScale = new Vector3 (x, transform.localScale.y, transform.localScale.z);
+
+
+	}
+}
