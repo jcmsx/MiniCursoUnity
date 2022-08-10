ae1ff8c56d6c3f692aba52bbe19ab768705acf10
jcmsx <jcmsx@yahoo.com.br>
2022-08-10 19:31:10 -0300

    camera - aula 7

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public Transform playerTransform;
+
+	public float speedCam;
+
+	private Camera cam;
+
+	public Transform limiteCamEsq, limiteCamDir, limiteCamSup, limiteCamBaixo;
+
+	// Use this for initialization
+	void Start () {
+
+		cam = Camera.main;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void LateUpdate()
+	{
+		//Vector3 posCam = new Vector3 (playerTransform.position.x, playerTransform.position.y, cam.transform.position.z);
+		//cam.transform.position = posCam;
+		CamController();
+
+	}
+
+
+
+	void CamController()
+	{
+
+		float posCamX = playerTransform.position.x;
+		float posCamY = playerTransform.position.y;
+
+		if (cam.transform.position.x < limiteCamEsq.position.x && playerTransform.position.x < limiteCamEsq.position.x)
+		{
+			posCamX = limiteCamEsq.position.x;
+		}
+
+
+		else if (cam.transform.position.x > limiteCamDir.position.x && playerTransform.position.x > limiteCamDir.position.x)
+		{
+			posCamX = limiteCamDir.position.x;
+		}
+
+
+
+		if (cam.transform.position.y < limiteCamBaixo.position.y && playerTransform.position.y < limiteCamBaixo.position.y)
+		{
+			posCamY = limiteCamBaixo.position.y;
+		}
+
+
+		else if (cam.transform.position.y > limiteCamSup.position.y && playerTransform.position.y > limiteCamSup.position.y)
+		{
+			posCamY = limiteCamSup.position.y;
+		}
+
+
+
+		Vector3 posCam = new Vector3 (posCamX, posCamY, cam.transform.position.z);
+
+		cam.transform.position = Vector3.Lerp (cam.transform.position, posCam, speedCam * Time.deltaTime);
+
+		//parou aula 8
+
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,25 +4,46 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
+	private GameController _GameController;
 
 	private Rigidbody2D playerRb;
+	private Animator playerAnimator;
+
 
 	public float speed;
 	public float jumpForce;
 
 	public bool isLookLeft;
 
+	public Transform groundCheck;
+	private bool isGrounded;
+	private bool isAtack;
+
+	public Transform mao;
+	public GameObject hitBoxPrefab;
+
 	// Use this for initialization
 	void Start () {
 	
 		playerRb = GetComponent<Rigidbody2D> ();
 
+		playerAnimator = GetComponent<Animator> ();
+
+		_GameController = FindObjectOfType (typeof(GameController)) as GameController;
+
+		_GameController.playerTransform = this.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		float h = Input.GetAxisRaw ("Horizontal");
+
+		if (isAtack == true && isGrounded == true) 
+		{
+			h = 0;
+		}
+
 
 
 		if (h > 0 && isLookLeft == true) {
@@ -35,14 +56,32 @@ public class playerController : MonoBehaviour {
 
 		float speedY = playerRb.velocity.y;
 
-		if (Input.GetButtonDown ("Jump")) {
+		if (Input.GetButtonDown ("Jump") && isGrounded == true) {
 
 			playerRb.AddForce( new Vector2(0,jumpForce));
 
 		}
+
+		if (Input.GetButtonDown ("Fire1") && isAtack == false) {
+		
+			isAtack = true;
+			playerAnimator.SetTrigger ("atack");
+		}
 	
 
 		playerRb.velocity = new Vector2 (h * speed, speedY);
+	
+		playerAnimator.SetInteger("h", (int) h);  // casting conversao int
+		playerAnimator.SetBool("isGrounded", isGrounded);
+		playerAnimator.SetFloat ("speedY", speedY);
+		playerAnimator.SetBool ("isAtack", isAtack);
+	
+	}
+
+	void FixedUpdate(){
+
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, 0.02f);
+
 	}
 
 	void Flip()
@@ -57,4 +96,52 @@ public class playerController : MonoBehaviour {
 
 
 	}
+
+	void OnEndAtack(){
+	
+		isAtack = false;
+	}
+
+	void hitBoxAtack()
+	{
+		GameObject hitBoxTemp = Instantiate (hitBoxPrefab, mao.position, transform.localRotation);
+		Destroy (hitBoxTemp, 0.2f);
+
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
